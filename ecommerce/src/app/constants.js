b5c6ffd42a4f5c56727cbc3ae18774c988f9fe58
Sahil <sahifindmyphone@gmail.com>
2024-04-14 04:28:38 +0530

    filter fixed + render API using

--- ecommerce/src/app/constants.js
+++ ecommerce/src/app/constants.js
@@ -1,6 +1,6 @@
 export const ITEMS_PER_PAGE = 9
-export const END_POINT = "http://localhost:8080"
-// export const END_POINT = 'https://ecommerce-lun6.onrender.com'
+// export const END_POINT = "http://localhost:8080"
+export const END_POINT = 'https://ecommerce-lun6.onrender.com'
 
 export function discountedPrice(item) {
     return Math.round(item.price * (1 - item.discountPercentage / 100), 2)
