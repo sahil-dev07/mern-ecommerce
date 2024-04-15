9eeb4450f08537d9fcea7632b17a76806d1e9686
Sahil <sahifindmyphone@gmail.com>
2024-04-15 10:14:59 +0530

    pattern comment

--- ecommerce/src/features/auth/component/Signup.jsx
+++ ecommerce/src/features/auth/component/Signup.jsx
@@ -55,7 +55,7 @@ export default function Signup() {
                                     id="email"
                                     {...register('email', {
                                         required: "Email is required"
-                                        , pattern: { value: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi, message: "Invalid Email" }
+                                        // , pattern: { value: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi, message: "Invalid Email" }
                                     })}
                                     type="email"
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
